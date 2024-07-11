0ac8858f1aac5e4c5e96880f411afff0bf67aab0
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-11 18:00:22 -0500

    Se crea la clase nodo con los atributos necesarios, se implementa la interfaz comparable para el ordenamiento  posterior de los nodos

---
+++ Huffman/Huffman.java
@@ -0,0 +1,5 @@
+public class Huffman{
+   public static void main(String[] args) {
+      System.out.println("Todo HONOR Y TODA GLORIA PARA DIOS");
+   }
+}
---
+++ Huffman/Node.java
@@ -0,0 +1,6 @@
+public class Node implements Comparable{
+   int frecuecy;
+   char c;
+   Node right;
+   Node left;
+}
