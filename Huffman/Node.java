b238274d78588b014ccd16fd501b2309773bbb23
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-11 18:03:18 -0500

    Se implementa el motodo compareTo para comparar las frecuencias en los nodods

--- Huffman/Node.java
+++ Huffman/Node.java
@@ -1,6 +1,10 @@
-public class Node implements Comparable{
+public class Node implements Comparable<Node>{
    int frecuecy;
    char c;
    Node right;
    Node left;
+
+   public int compareTo(Node node){
+      return this.frecuecy - node.frecuecy;
+   }
 }
