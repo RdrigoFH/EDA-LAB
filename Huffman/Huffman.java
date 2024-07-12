0d0aac1d4b68921ab25c1fabd3fa19b589f9129a
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-11 19:01:54 -0500

    Se implementa el metodo para la creacion del arbol de huffman

--- Huffman/Huffman.java
+++ Huffman/Huffman.java
@@ -1,5 +1,23 @@
+import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Huffman{
+   PriorityQueue<Node> queue = new PriorityQueue<>(); 
+
+   public void huffmanTree(HashMap<Character, Integer> map){
+   // creamos los nodos para cada parte del mapa
+      for(Map.Entry<Character, Integer> entry : map.entrySet())
+         queue.add(new Node(entry.getValue(), entry.getKey()));
+
+      // solo puede haber un nodo raiz, el resto se convertiran en hijos
+      while(queue.size() > 1){
+         
+      }
+   }
+
+
    public static void main(String[] args) {
-      System.out.println("Todo HONOR Y TODA GLORIA PARA DIOS");
+
    }
 }
