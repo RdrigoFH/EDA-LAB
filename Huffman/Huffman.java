d8175dead06b8042382f8aea69137604cc4b27ea
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-11 20:41:14 -0500

    termina el algoritmo de Huffman

--- Huffman/Huffman.java
+++ Huffman/Huffman.java
@@ -23,12 +23,36 @@ public class Huffman{
    public HashMap<Character, Integer> buildMap(String str){
       HashMap<Character, Integer> map = new HashMap<>();
       for(char ch : str.toCharArray()){
-         map.compute(ch, (key,value) -> (key== null)? 1 : value + 1);
+         map.compute(ch, (key,value) -> (value== null)? 1 : value + 1);
       }
       return map;
    }  
 
-   public static void main(String[] args) {
+   public HashMap<Character, String> generateCodes(Node huffmanTree){
+      HashMap<Character, String> codeMap = new HashMap<>();
+      generateCodes(huffmanTree,"",codeMap);
+      return codeMap;
+   }
+   public void generateCodes(Node node, String code, Map<Character, String> huffmanCode) {
+      if (node == null) {
+          return;
+      }
 
+      if (node.left == null && node.right == null) {
+          huffmanCode.put(node.c, code);
+      }
+
+      generateCodes(node.left, code + '0', huffmanCode);
+      generateCodes(node.right, code + '1', huffmanCode);
+   }
+   public HashMap compres(String str){
+      HashMap map = buildMap(str); 
+      Node node = huffmanTree(map);
+      return generateCodes(node);
+   }
+   public static void main(String[] args) {
+      String str = "abecedario, cirstiano ronaldo, lionel messi, bobo, colombia a ganar la copa america ";
+      Huffman h = new Huffman();
+      System.out.println("los caracteres comprimidos son: \n" + h.compres(str));
    }
 }
