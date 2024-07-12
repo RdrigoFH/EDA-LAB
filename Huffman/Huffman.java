ff277106d7b8e6115c140319e9788cb19735622e
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-11 19:16:06 -0500

    Se retorna el arbol de huffman

--- Huffman/Huffman.java
+++ Huffman/Huffman.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Huffman{
    PriorityQueue<Node> queue = new PriorityQueue<>(); 
 
-   public void huffmanTree(HashMap<Character, Integer> map){
+   public Node huffmanTree(HashMap<Character, Integer> map){
    // creamos los nodos para cada parte del mapa
       for(Map.Entry<Character, Integer> entry : map.entrySet())
          queue.add(new Node(entry.getValue(), entry.getKey()));
@@ -17,6 +17,7 @@ public class Huffman{
          Node fusion = new Node(left.frequecy+right.frequecy, '\0', left, right);
          queue.add(fusion);
       }
+      return queue.poll();
    }
 
 
