c216b9155fec7d56673c32b1274d65d99b64a006
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-11 19:11:38 -0500

    Se crea el constructor personalizado que contenga todos los q=argumentos

--- Huffman/Node.java
+++ Huffman/Node.java
@@ -1,13 +1,18 @@
 public class Node implements Comparable<Node>{
-   private int frequecy;
-   private char c;
-   private Node right;
-   private Node left;
+   protected int frequecy;
+   protected char c;
+   protected Node right;
+   protected Node left;
 
    public int compareTo(Node node){
       return this.frequecy - node.frequecy;
    }
    public Node(int frequency, char c){
       this.frequecy = frequency; this.c = c;
+   }
+   public Node(int frequency, char c, Node left, Node right){
+      this.frequecy = frequency; this.c = c;
+      this.right = right;
+      this.left = left;
    }
 }
